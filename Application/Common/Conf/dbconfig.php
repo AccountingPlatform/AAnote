bdaee3d6b66a4f5cdd19220d0d53ebf018107a8a
CodeApePro <codeape@outlook.com>
2015-06-05 15:01:23 +0800

    检测排除方法是否生效

--- Application/Common/Conf/dbconfig.php
+++ Application/Common/Conf/dbconfig.php
@@ -5,7 +5,7 @@
  */
 return array(
     /* 数据库配置 */
-    'DB_TYPE' => '', // 数据库类型
+    'DB_TYPE' => 'mysql', // 数据库类型
     'DB_HOST' => '', // 服务器地址
     'DB_NAME' => '', // 数据库名
     'DB_USER' => '', // 用户名
